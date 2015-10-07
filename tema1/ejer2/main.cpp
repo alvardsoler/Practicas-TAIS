a39a4f842bd2da4ab5d9f5a8be5f4acb734c4ed0
alvardsoler <aigam666@gmail.com>
2015-10-07 18:30:03 +0200

    Ejercicio dos acabado y comentado

--- tema1/ejer2/main.cpp
+++ tema1/ejer2/main.cpp
@@ -1,130 +1,99 @@
-
+/*
+ * Grupo TAIS16, Samuel Lapuente Jiménez, Alvar David Soler Rus
+ * Solución: una función recursiva que recorre el árbol comprobando que está ordenado (hijos izquierdos
+ * menores que la raiz del padre e hijos derechos mayores que la raiz del padre) y que está balanceado
+ * (la diferencia entre alturas de las ramas no puede ser mayor que uno).
+ * 
+ *  */
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <climits>
 #include <algorithm>
 #include "BinTree.h"
 
 /**
-* Creación del árbol binario
-* @return BinTree construido
-*/
-
-/* Vamos a aprovechar nuestra solución del ejercicio anterior ya que una de las condiciones necesarias es que el árbol sea un árbol equilibrado.
-Como esta situación la tenemos contemplada en estas funciones y ya es correcto, podremos centrarnos en la otra condición para que sea un árbol AVL, esto es, 
-que sea de búsqueda (los elementos de la izquierda menores que la ráíz y los de la derecha mayores)*/
-
+ * 
+ * Creación del árbol binario
+ * @return BinTree construido
+ */
 BinTree<int> leerArbol() {
-	int raiz;
-	std::cin >> raiz;
-	if (raiz == -1) {
-		// es un árbol vacío
-		return
-		{};
-	}
-	else {
-		// leer recursivamente los hijos
-		auto iz = leerArbol();
-		auto dr = leerArbol();
-		return
-		{
-			iz, raiz, dr
-		};
-	}
+    int raiz;
+    std::cin >> raiz;
+    if (raiz == -1) {
+	return
+	{
+	};
+    } else {
+	auto iz = leerArbol();
+	auto dr = leerArbol();
+	return
+	{
+	    iz, raiz, dr
+	};
+    }
 }
 
-int altura(BinTree<int> const & arbol) {
-	if (arbol.empty()) {
-		return 0;
-	}
-	else {
-		return 1 + std::max(altura(arbol.left()), altura(arbol.right()));
-	}
-}
-
-bool resuelveAltura(BinTree<int> const & arbol) {
-	if (arbol.empty()) return true;
-	if (resuelveAltura(arbol.left()) && resuelveAltura(arbol.right())) {
-		int alturaDer = altura(arbol.right());
-		int alturaIzq = altura(arbol.left());
-		if (std::abs(alturaDer - alturaIzq) <= 1) return true;
-		else return false;
-	}
-	else {
-		return false;
-	}
-}
-
-
-/*Con este método vamos comprobando que la parte derecha del árbol es mayor que su raíz. Es necesario comprobar dos veces que no sea vacío tal y como lo estamos implementando para no entrar en la excepción*/
-
-bool mayor(BinTree<int> const & arbol) {
-	if (arbol.empty()) {
+/**
+ * Función principal. Si la raiz es vacía no hay nada que hacer. En otro caso,
+ * comprobamos que la raiz está entre el valor mínimo y máximo (inicializados como el máximo  y mínimo 
+ * valor posible de una variable int), y si es así comprobamos que la diferencia de alturas de las dos ramas
+ * cumple la condición para ser balanceado. En caso de que alguna de estas condiciones no se cumpla, 
+ * sabemos que el árbol no es AVL.  Complejidad O(n) siendo n el número de nodos.
+ * @param arbol que se quiere analizar
+ * @param alt altura maxima de las ramas del arbol
+ * @param min valor mínimo entre  el que se tiene que encontrar la raiz
+ * @param max valor maximo entre  el que se tiene que encontrar la raiz
+ * @return si el arbol es AVL o no
+ */
+bool resuelve(BinTree<int> const & arbol, int &alt, int min, int max) {
+    if (arbol.empty()) {
+	return true;
+    } else {
+	if (arbol.root() < max && arbol.root() > min) {
+	    int auxI, auxD;
+	    auxI = alt;
+	    auxD = alt;
+	    if (!resuelve(arbol.left(), auxI, min, arbol.root())) return false;
+	    if (!resuelve(arbol.right(), auxD, arbol.root(), max)) return false;
+	    if (std::abs(auxI - auxD) <= 1) {
+		alt = 1 + std::max(auxI, auxD);
 		return true;
-	}
-	else if (!arbol.right().empty()) {
-		if (arbol.right().root() < arbol.root()) return false;
-		else return mayor(arbol.right());
-	}
-	else return true;
-}  
-
-/*Este método es equivalente a mayor pero mirando que la parte izquierda sea menor que la raíz*/
-
-bool menor(BinTree<int> const & arbol) {
-	if (arbol.empty()) {
-		return true;
-	}
-	else if (!arbol.left().empty()) {
-		if (arbol.left().root() > arbol.root()) return false;
-		else return menor(arbol.left());
-	}
-	else return true;
-}
-
-/*Ahora comenzamos a comprobar que sea de búsqueda. Este método ya comprueba que además esté balanceado, si no devuelve false*/
-
-bool resuelveBusqueda(BinTree<int> const & arbol) {
-	if (resuelveAltura(arbol) == true) {
-		if (arbol.empty()) return true;
-		if (resuelveBusqueda(arbol.left()) && resuelveBusqueda(arbol.right())) {
-			if ((mayor(arbol.right())) && menor(arbol.left())) return true;
-			else return false;
-		}
-	}
-	else return false;
+	    } else return false;
+	} else return false;
+    }
 }
 
 void resolverCaso() {
-	auto arbol = leerArbol();
-	if (resuelveBusqueda(arbol)) {
-		std::cout << "SI" << std::endl;
-	}
-	else {
-		std::cout << "NO" << std::endl;
-	}
+    auto arbol = leerArbol();
+    int a = 0;
+    if (resuelve(arbol, a, INT_MIN, INT_MAX)) {
+	std::cout << "SI" << std::endl;
+    } else {
+	std::cout << "NO" << std::endl;
+    }
 }
 
 int main() {
 #ifndef DOMJUDGE
 
-	std::ifstream in("caso.txt"); 
+    std::ifstream in("caso.txt");
 
-	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 
 #endif
-	int numCasos;
-	std::cin >> numCasos;
-	for (int i = 0; i < numCasos; ++i)
-		resolverCaso();
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+	resolverCaso();
 
 #ifndef DOMJUDGE
 
-	std::cin.rdbuf(cinbuf); //reset to standard input again
+    std::cin.rdbuf(cinbuf); //reset to standard input again
 
-	system("PAUSE");
+    system("PAUSE");
 
 #endif
 
-	return 0;
+    return 0;
 }
