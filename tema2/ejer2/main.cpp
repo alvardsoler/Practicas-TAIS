9ef7a717eb3729856ced386f6f3201926349676e
alvardsoler <aigam666@gmail.com>
2015-10-24 14:57:13 +0200

    Cambios en 7. Ahora está mejor usando unsigned long int y más limpio pero sigue sin pasar

--- tema2/ejer2/main.cpp
+++ tema2/ejer2/main.cpp
@@ -11,36 +11,44 @@
 #include <cstdlib>
 #include "PriorityQueue.h"
 
+PriorityQueue<unsigned long int, std::less<unsigned int>> leer(int const n) {
+    PriorityQueue<unsigned long int, std::less<unsigned int>> ret;
+    int aux;
+    for (int i = 0; i < n; i++) {
+	std::cin >> aux;
+	ret.push(aux);
+    }
+    return ret;
+}
+
+unsigned long resuelve(PriorityQueue<unsigned long int, std::less<unsigned int>> cola) {
+    PriorityQueue<unsigned long int, std::less<unsigned int>> colaAux;
+    unsigned long int aux1;
+    unsigned long int aux2;
+    while (cola.size() > 1) {
+	aux1 = cola.top();
+	cola.pop();
+	aux2 = cola.top();
+	cola.pop();
+	colaAux.push((unsigned long) (aux1 + aux2));
+	cola.push((unsigned long) (aux1 + aux2));
+    }
+    while (colaAux.size() > 1) {
+	aux1 = colaAux.top();
+	colaAux.pop();
+	aux2 = colaAux.top();
+	colaAux.pop();
+	colaAux.push((unsigned long) (aux1 + aux2));
+    }
+    return colaAux.top();
+}
+
 void resolverCaso() {
     int n;
     std::cin >> n;
-    unsigned long aux1;
-    unsigned long aux2;
-
     while (n != 0) {
-	PriorityQueue<unsigned long, std::less<unsigned int>> cola;
-	PriorityQueue<unsigned long, std::less<unsigned int>> colaAux;
-	int aux;
-	for (int i = 0; i < n; i++) {
-	    std::cin >> aux;
-	    cola.push(aux);
-	}
-	while (cola.size() > 1) {
-	    aux1 = cola.top();
-	    cola.pop();
-	    aux2 = cola.top();
-	    cola.pop();
-	    colaAux.push((aux1 + aux2));
-	    cola.push((aux1 + aux2));
-	}
-	while (colaAux.size() > 1) {
-	    aux1 = colaAux.top();
-	    colaAux.pop();
-	    aux2 = colaAux.top();
-	    colaAux.pop();
-	    colaAux.push((aux1 + aux2));
-	}
-	std::cout << colaAux.top() << std::endl;
+	auto cola = leer(n);
+	std::cout << resuelve(cola) << std::endl;
 	std::cin >> n;
     }
 }
@@ -58,10 +66,6 @@ int main() {
 #ifndef DOMJUDGE
 
     std::cin.rdbuf(cinbuf); //reset to standard input again
-
-    //char ch;
-
-    //std::cin >> ch;
 
     system("PAUSE");
 
