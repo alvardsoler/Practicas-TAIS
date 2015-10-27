74206d65ffb482b0b2f39cbc7997e9731fddcda5
alvardsoler <aigam666@gmail.com>
2015-10-27 23:49:00 +0100

    Ya no se q mas cambiar

--- tema2/ejer2/main.cpp
+++ tema2/ejer2/main.cpp
@@ -11,8 +11,8 @@
 #include <cstdlib>
 #include "PriorityQueue.h"
 
-PriorityQueue<unsigned long long int, std::less<unsigned int>> leer(int const n) {
-    PriorityQueue<unsigned long long int, std::less<unsigned int>> ret;
+PriorityQueue<unsigned long long int> leer(int const n) {
+    PriorityQueue<unsigned long long int,std::less<unsigned long long int>> ret;
     int aux;
     for (int i = 0; i < n; i++) {
 	std::cin >> aux;
@@ -21,27 +21,31 @@ PriorityQueue<unsigned long long int, std::less<unsigned int>> leer(int const n)
     return ret;
 }
 
-unsigned long resuelve(PriorityQueue<unsigned long long int, std::less<unsigned int>> &cola) {
-    if (cola.size() == 1) return cola.top();
-    PriorityQueue<unsigned long long int, std::less<unsigned long long int>> colaAux;
+unsigned long resuelve(PriorityQueue<unsigned long long int, std::less<unsigned long long int>> &cola) {
+    if (cola.size() == 1) return 0;
+//    PriorityQueue<unsigned long long int> colaAux;
     unsigned long long int aux1;
     unsigned long long int aux2;
+    unsigned long long int tot = 0;
     while (cola.size() > 1) {
 	aux1 = cola.top();
 	cola.pop();
 	aux2 = cola.top();
 	cola.pop();
-	colaAux.push((aux1 + aux2));
+//	colaAux.push((aux1 + aux2));
 	cola.push((aux1 + aux2));
+	tot += aux1 + aux2;
     }
-    while (colaAux.size() > 1) {
-	aux1 = colaAux.top();
-	colaAux.pop();
-	aux2 = colaAux.top();
-	colaAux.pop();
-	colaAux.push((aux1 + aux2));
-    }
-    return colaAux.top();
+//    while (colaAux.size() > 1) {
+//	aux1 = colaAux.top();
+//	colaAux.pop();
+//	aux2 = colaAux.top();
+//	colaAux.pop();
+//	colaAux.push((aux1 + aux2));
+//    }
+
+//    return colaAux.top();
+    return tot;
 }
 
 void resolverCaso() {
