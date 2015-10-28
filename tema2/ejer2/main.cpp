a125f1ded9f42b7f01ff81b455431c79d03a0f69
alvardsoler <aigam666@gmail.com>
2015-10-28 20:53:09 +0100

    resuelto

--- tema2/ejer2/main.cpp
+++ tema2/ejer2/main.cpp
@@ -1,9 +1,9 @@
 /*
- * Grupo TAIS16, Samuel Lapuente Jiménez, Alvar David Soler Rus
- * 
- * Ejercicio 6. Tema 2.
- * 
- */
+* Grupo TAIS16, Samuel Lapuente Jiménez, Alvar David Soler Rus
+*
+* Ejercicio 7. Tema 2.
+*
+*/
 
 
 #include <iostream>
@@ -12,69 +12,58 @@
 #include "PriorityQueue.h"
 
 PriorityQueue<unsigned long long int> leer(int const n) {
-    PriorityQueue<unsigned long long int,std::less<unsigned long long int>> ret;
-    int aux;
-    for (int i = 0; i < n; i++) {
-	std::cin >> aux;
-	ret.push(aux);
-    }
-    return ret;
+	PriorityQueue<unsigned long long int> ret;
+	int aux;
+	for (int i = 0; i < n; i++) {
+		std::cin >> aux;
+		ret.push(aux);
+	}
+	return ret;
 }
 
-unsigned long resuelve(PriorityQueue<unsigned long long int, std::less<unsigned long long int>> &cola) {
-    if (cola.size() == 1) return 0;
-//    PriorityQueue<unsigned long long int> colaAux;
-    unsigned long long int aux1;
-    unsigned long long int aux2;
-    unsigned long long int tot = 0;
-    while (cola.size() > 1) {
-	aux1 = cola.top();
-	cola.pop();
-	aux2 = cola.top();
-	cola.pop();
-//	colaAux.push((aux1 + aux2));
-	cola.push((aux1 + aux2));
-	tot += aux1 + aux2;
-    }
-//    while (colaAux.size() > 1) {
-//	aux1 = colaAux.top();
-//	colaAux.pop();
-//	aux2 = colaAux.top();
-//	colaAux.pop();
-//	colaAux.push((aux1 + aux2));
-//    }
-
-//    return colaAux.top();
-    return tot;
+unsigned long long int resuelve(PriorityQueue<unsigned long long int> &cola) {
+	if (cola.size() == 1) return 0;
+	unsigned long long int aux1;
+	unsigned long long int aux2;
+	unsigned long long int total = 0;
+	while (cola.size() > 1) {
+		aux1 = cola.top();
+		cola.pop();
+		aux2 = cola.top();
+		cola.pop();
+		cola.push((aux1 + aux2));
+		total += aux1 + aux2;
+	}
+	return total;
 }
 
 void resolverCaso() {
-    int n;
-    std::cin >> n;
-    while (n != 0) {
-	auto cola = leer(n);
-	std::cout << resuelve(cola) << std::endl;
+	int n;
 	std::cin >> n;
-    }
+	while (n != 0) {
+		auto cola = leer(n);
+		std::cout << resuelve(cola) << std::endl;
+		std::cin >> n;
+	}
 }
 
 int main() {
 #ifndef DOMJUDGE
 
-    std::ifstream in("casos.txt");
+	std::ifstream in("casos.txt");
 
-    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 
 #endif
-    resolverCaso();
+	resolverCaso();
 
 #ifndef DOMJUDGE
 
-    std::cin.rdbuf(cinbuf); //reset to standard input again
+	std::cin.rdbuf(cinbuf); //reset to standard input again
 
-    system("PAUSE");
+	system("PAUSE");
 
 #endif
 
-    return 0;
+	return 0;
 }
