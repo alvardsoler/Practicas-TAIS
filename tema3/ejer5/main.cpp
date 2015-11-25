3123d6a555683953eec6e877c62bcf1d0d547cc5
Samu <slapuente24@gmail.com>
2015-11-25 11:07:10 +0100

    añadido control para el caso base

--- tema3/ejer5/main.cpp
+++ tema3/ejer5/main.cpp
@@ -1,9 +1,9 @@
 /*
- * Grupo TAIS16, Samuel Lapuente Jiménez, Alvar David Soler Rus
- * 
- * Ejercicio 11. Tema 3.
- * 
- */
+* Grupo TAIS16, Samuel Lapuente Jiménez, Alvar David Soler Rus
+*
+* Ejercicio 11. Tema 3.
+*
+*/
 
 
 #include <iostream>
@@ -14,69 +14,76 @@
 #include "Manchas.h"
 
 /**
- * Creación del grafo
- * @return Grafo construido
- */
-Grafo leerGrafo(int const _f) {
-    int _c;
-    char _m;
-    std::cin >> _c;
-    std::vector < std::vector<bool>> _marked(_f, std::vector<bool>(_c, false));
-    Grafo g(_f * _c);
-    std::cin.get(_m);    
-    for (int i = 0; i < _f; ++i) {
-	for (int j = 0; j < _c; ++j) {
-	    std::cin.get(_m);
-	    if (_m == '#') {
-		_marked[i][j] = true;
-		if (i > 0) {
-		    if (_marked[i - 1][j]) {			
-			g.ponArista(i * _c + j, ((i - 1) * _c) + j);
-		    }
-
-		}
-		if (j > 0)
-		    if (_marked[i][j - 1]) {
-			g.ponArista(i * _c + j, i * _c + (j - 1));
-		    }
-	    }
-	}
+* Creación del grafo
+* @return Grafo construido
+*/
+Grafo leerGrafo(int const _f, bool  & hayManchas) {
+	int _c;
+	char _m; 
+	std::cin >> _c;
+	std::vector < std::vector<bool>> _marked(_f, std::vector<bool>(_c, false));
+	Grafo g(_f * _c);
 	std::cin.get(_m);
-    }
+	for (int i = 0; i < _f; ++i) {
+		for (int j = 0; j < _c; ++j) {
+			std::cin.get(_m);
+			if (_m == '#') {
+				hayManchas = true;
 
-    return g;
+				_marked[i][j] = true;
+				if (i > 0) {
+					if (_marked[i - 1][j]) {
+						g.ponArista(i * _c + j, ((i - 1) * _c) + j);
+					}
+
+				}
+				if (j > 0)
+					if (_marked[i][j - 1]) {
+						g.ponArista(i * _c + j, i * _c + (j - 1));
+					}
+			}
+		}
+		std::cin.get(_m);
+	}
+	
+	
+	return g;
 
 }
 
 void resolverCaso() {
-    int f;
-    std::cin >> f;
-    while (std::cin) {
-	auto grafo = leerGrafo(f);
-	Manchas manchas(grafo);
-	std::cout << manchas.getComponenteMayor() << std::endl;
+	int f;
 	std::cin >> f;
-    }
+	while (std::cin) {
+		bool hayManchas = false;
+		auto grafo = leerGrafo(f,hayManchas);
+		if (!hayManchas) std::cout << 0 << std::endl;
+		else {
+			Manchas manchas(grafo);
+			std::cout << manchas.getComponenteMayor() << std::endl;
+		}
+		std::cin >> f;
+	}
 }
 
 int main() {
 #ifndef DOMJUDGE
 
-    std::ifstream in("casos.txt");
+	std::ifstream in("casos.txt");
 
-    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 
 #endif
 
-    resolverCaso();
+	resolverCaso();
 
 #ifndef DOMJUDGE
 
-    std::cin.rdbuf(cinbuf); //reset to standard input again
+	std::cin.rdbuf(cinbuf); //reset to standard input again
 
-    system("PAUSE");
+	system("PAUSE");
 
 #endif
 
-    return 0;
+	return 0;
 }
