8a4c8aa2952d76dfe0a52ddee5e2d43874af84e0
Samu <slapuente24@gmail.com>
2015-11-25 10:43:27 +0100

    pasa el juez

--- tema3/ejer4/SerpientesEscaleras.h
+++ tema3/ejer4/SerpientesEscaleras.h
@@ -1,6 +1,6 @@
 /*
 * File:   ArbolBipartito.h
-* Author: Samuel Lapuente Jiménez y Alvar David Soler Rus
+* Author: Samuel Lapuente JimÃ©nez y Alvar David Soler Rus
 *
 * Created on 13 de noviembre de 2015, 14:12
 */
@@ -13,40 +13,26 @@
 
 class SerpientesEscaleras {
 private:
-	GrafoDirigido grafo;
+	
 	int saltos;
-	std::vector<bool> _visited;
-	std::vector<bool> _colors;
+	
+	
 
-	void resuelve(std::vector<int> const & atajos) {
+	void resuelve(GrafoDirigido const & grafo) {
 		std::vector<size_t> vec = grafo.adj(0);
 		std::vector<size_t>::iterator it2 = vec.begin();
 		
 		BreadthFirstDirectedPaths bfdp(grafo, 0);
 
-		Path camino = bfdp.pathTo(grafo.V() - 1);
+		Path camino = bfdp.pathTo(grafo.V()-1);
 		std::deque<size_t>::iterator it = camino.begin();
 		saltos = camino.size() -1 ;
-		while (it != camino.end()){
-			//std::cout << *it << " ";
-			for (size_t k = 0; k < atajos.size(); ++k){
-				if (*it == atajos[k]){
-					--saltos; break;
-				}
-			}
-			it++;
-			
-		}
-		//std::cout << std::endl;
-
-
-		
 	}
 
 public:
 
-	SerpientesEscaleras(GrafoDirigido g, std::vector<int> const & atajos) : grafo(g), saltos(0), _visited(g.V(), false), _colors(g.V(), false) {
-		resuelve(atajos);
+	SerpientesEscaleras(GrafoDirigido const & g) : saltos(0) {
+		resuelve(g);
 	};
 
 	int getSaltos(){
