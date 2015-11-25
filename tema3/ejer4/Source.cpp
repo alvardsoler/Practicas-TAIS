0fcea31060e6d858996de7d029a9878590e7811a
Samu <slapuente24@gmail.com>
2015-11-25 10:43:53 +0100

    pasa juez

--- tema3/ejer4/Source.cpp
+++ tema3/ejer4/Source.cpp
@@ -1,5 +1,5 @@
 /*
-* Grupo TAIS16, Samuel Lapuente Jiménez, Alvar David Soler Rus
+* Grupo TAIS16, Samuel Lapuente JimÃ©nez, Alvar David Soler Rus
 *
 * Ejercicio 13. Tema 3.
 *
@@ -15,13 +15,26 @@
 
 
 /**
-* Creación del grafo
+* CreaciÃ³n del grafo
 * @return Grafo construido
 */
+struct Par {
+	int origen;
+	int destino;
+};
+
+int getDestino(int v, std::vector<Par> atajos){
+	for (Par par : atajos){
+		if (par.origen == v) return par.destino;
+	}
+}
+
+
 SerpientesEscaleras leerGrafo(int const _v) {
 	int s, e, v, w, k;
-	GrafoDirigido g(_v * _v);
-	std::vector<int> atajos;
+	int tam = _v * _v;
+	GrafoDirigido g(tam);
+	std::vector<Par> atajos;
 	std::vector<bool> hayAtajos(_v * _v, false);
 	std::cin >> k;
 	std::cin >> s;
@@ -29,30 +42,30 @@ SerpientesEscaleras leerGrafo(int const _v) {
 	for (int i = 0; i < s + e; ++i) {
 		std::cin >> v;
 		std::cin >> w;
-		g.ponArista(v, w);		
-		atajos.push_back(v);
-		hayAtajos[v] = true;
+		Par p;
+		p.origen = v - 1;
+		p.destino = w - 1;
+		atajos.push_back(p);
+		hayAtajos[v - 1] = true;
 	}
+	int paso;
 	for (int j = 0; j < g.V() - 1; ++j) {
-		if (!hayAtajos[j]){		
-		if (((g.V() - j - 1) / k) < 1) {
-			for (int z = 1; z < (g.V() - j); ++z) {
-				g.ponArista(j, j + z);
+		if (!hayAtajos[j]){
+			if ((g.V() - j) > k){
+				paso = k;
+			}
+			else paso = g.V() - j;
+
+			for (int z = 1; z <= paso && j+z < tam; ++z){
+				if (!hayAtajos[j + z]) g.ponArista(j, j + z);
+				else {
+					g.ponArista(j, getDestino(j + z, atajos));
+				}
 			}
 
 		}
-		else
-			for (int z = 1; z <= k; ++z) {
-				g.ponArista(j, j + z);
-			}
-		}
 	}
-	//    for (int j = g.V() - k - 1; j < g.V() - 1; ++j) {
-	//	for (int z = 1; z < j; ++z) {
-	//	    g.ponArista(j, j + z);
-	//	}
-	//    }
-	SerpientesEscaleras se(g, atajos);
+	SerpientesEscaleras se(g);
 	return se;
 
 }
@@ -62,12 +75,14 @@ void resolverCaso() {
 	std::cin >> v;
 	while (v != 0) {
 		SerpientesEscaleras se = leerGrafo(v);
-		
+
 		std::cout << se.getSaltos() << std::endl;
 		std::cin >> v;
 
 	}
 }
+
+
 
 int main() {
 #ifndef DOMJUDGE
