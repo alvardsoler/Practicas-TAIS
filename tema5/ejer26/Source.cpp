f747e7c0634797e7b027556706be5942b4cf88d2
alvardsoler <aigam666@gmail.com>
2016-01-20 15:04:41 +0100

    Ejer26 pasa

--- tema5/ejer26/Source.cpp
+++ tema5/ejer26/Source.cpp
@@ -1,26 +1,27 @@
 /**
-* TAIS16. �lvar Soler y Samuel Lapuente
-* Ejercicio 25.
+* TAIS16. Álvar Soler y Samuel Lapuente
+* Ejercicio 26.
 *
 **/
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include "Matriz.h"
 
 
 /*
-* posible(i,j) devuelve si se puede construir la varilla de tama�o j usando varillas desde la 1 hasta la i.
+* posible(i,j) devuelve si se puede construir la varilla de tamaño j usando varillas desde la 1 hasta la i.
 * posible(i-1,j) si li>j
 * posible(i-1,j) || posible(i-1,j-li) si li<=j
 * C.B. posible(0,j) = false
 * C.B. posible(i,0) = true
-* Llamada inicial posible(n,L) (que es donde estar� nuestra soluci�n en la tabla)
-* la tabla se genera de izquierda a derecha y de arriba a abajo, ya que necesitamos las casillas de encima y encima a la izquierda para generar la 
+* Llamada inicial posible(n,L) (que es donde estará nuestra solución en la tabla)
+* la tabla se genera de izquierda a derecha y de arriba a abajo, ya que necesitamos las casillas de encima y encima a la izquierda para generar la
 * casilla actual
 */
 bool esPosible(std::vector<int> const &varillas, const int L) {
-	Matriz<bool> posible(varillas.size(), L+1, false);
+	Matriz<bool> posible(varillas.size(), L + 1, false);
 	for (size_t i = 0; i < varillas.size(); ++i) {
 		posible[i][0] = true;
 	}
@@ -30,12 +31,99 @@ bool esPosible(std::vector<int> const &varillas, const int L) {
 				posible[i][j] = posible[i - 1][j];
 			}
 			else {
-				posible[i][j] = (posible[i - 1][j]) ? posible[i - 1][j] : posible[i - 1][j-varillas[i]];
+				posible[i][j] = (posible[i - 1][j]) ? posible[i - 1][j] : posible[i - 1][j - varillas[i]];
 
 			}
 		}
 	}
 	return posible[varillas.size() - 1][L];
+}
+
+/*
+* formas(i,j) devuelve la cantidad de formas de construir una varilla de tamaño j usando varillas desde la 1 hasta la i.
+* formas(i-1,j) si li>j
+* formas(i-1,j) + formas(i-1,j-li) si li<=j
+* C.B. formas(0,j) = 0
+* C.B. formas(i,0) = 1
+* Llamada inicial formas(n,L) (que es donde estará nuestra solución en la tabla)
+* la tabla se genera de izquierda a derecha y de arriba a abajo, ya que necesitamos las casillas de encima y encima a la izquierda para generar la
+* casilla actual
+*/
+int formas(std::vector<int> const &varillas, const int L) {
+	Matriz<int> formas(varillas.size(), L + 1, 0);
+	for (size_t i = 0; i < varillas.size(); ++i) {
+		formas[i][0] = 1;
+	}
+	for (size_t i = 1; i <= varillas.size() - 1; ++i) {
+		for (size_t j = 1; j <= L; ++j) {
+			if (varillas[i] > j) {
+				formas[i][j] = formas[i - 1][j];
+			}
+			else {
+				formas[i][j] = formas[i - 1][j] + formas[i - 1][j - varillas[i]];
+
+			}
+		}
+	}
+	return formas[varillas.size() - 1][L];
+}
+
+/*
+* nVarillas(i,j) devuelve el minimo número de varillas para construir una varilla de tamaño j usando varillas desde la 1 hasta la i.
+* nVarillas(i-1,j) si li>j
+* minimo (nVarillas(i-1,j), nVarillas(i-1,j-li) si li<=j)
+* C.B. nVarillas(0,j) = infinito
+* C.B. nVarillas(i,0) = 0
+* Llamada inicial nVarillas(n,L) (que es donde estará nuestra solución en la tabla)
+* la tabla se genera de izquierda a derecha y de arriba a abajo, ya que necesitamos las casillas de encima y encima a la izquierda para generar la
+* casilla actual
+*/
+
+int nVarillas(std::vector<int> const &varillas, const int L) {
+	Matriz<int> nVarillas(varillas.size(), L + 1, 1e9);
+	for (size_t i = 0; i < varillas.size(); ++i) {
+		nVarillas[i][0] = 0;
+	}
+	for (size_t i = 1; i <= varillas.size() - 1; ++i) {
+		for (size_t j = 1; j <= L; ++j) {
+			if (varillas[i] > j) {
+				nVarillas[i][j] = nVarillas[i - 1][j];
+			}
+			else {
+				nVarillas[i][j] = std::min(nVarillas[i - 1][j], nVarillas[i - 1][j - varillas[i]] + 1);
+			}
+		}
+	}
+	return nVarillas[varillas.size() - 1][L];
+}
+
+/*
+* precio(i,j) devuelve el minimo precio para construir una varilla de tamaño j usando varillas desde la 1 hasta la i.
+* precio(i-1,j) si li>j
+* minimo (precio(i-1,j), precio(i-1,j-li) si li<=j)
+* C.B. precio(0,j) = infinito
+* C.B. precio(i,0) = 0
+* Llamada inicial precio(n,L) (que es donde estará nuestra solución en la tabla)
+* la tabla se genera de izquierda a derecha y de arriba a abajo, ya que necesitamos las casillas de encima y encima a la izquierda para generar la
+* casilla actual
+*/
+
+int precio(std::vector<int> const &varillas, const int L, std::vector<int> const &precios) {
+	Matriz<int> precio(varillas.size(), L + 1, 1e9);
+	for (size_t i = 0; i < varillas.size(); ++i) {
+		precio[i][0] = 0;
+	}
+	for (size_t i = 1; i <= varillas.size() - 1; ++i) {
+		for (size_t j = 1; j <= L; ++j) {
+			if (varillas[i] > j) {
+				precio[i][j] = precio[i - 1][j];
+			}
+			else {
+				precio[i][j] = std::min(precio[i - 1][j], precio[i - 1][j - varillas[i]] + precios[i]);
+			}
+		}
+	}
+	return precio[varillas.size() - 1][L];
 }
 
 int main() {
@@ -44,15 +132,21 @@ int main() {
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 
 #endif
-	int n, L, a, b;
+	int n, L;
 	std::cin >> n >> L;
-	std::vector<int> v(n+1,0);
-	for (size_t i = 1; i < n+1; ++i) {
+	while (std::cin) {
+	std::vector<int> v(n + 1, 0);
+	std::vector<int> p(n + 1, 0);
+	for (size_t i = 1; i < n + 1; ++i) {
 		std::cin >> v[i];
-		std::cin >> a;
+		std::cin >> p[i];
 	}
-	if (esPosible(v,L)) std::cout << "si" << std::endl;
-	else std::cout << "no" << std::endl;
+
+	if (esPosible(v, L)) std::cout << "SI " << formas(v, L) << " " << nVarillas(v, L) << " " << precio(v, L, p) << std::endl;
+	else std::cout << "NO" << std::endl;
+	std::cin >> n >> L;
+	}
+
 
 #ifndef DOMJUDGE
 
