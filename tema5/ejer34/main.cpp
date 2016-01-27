b613e28706e5dab646ff7e315d043d1dd1740c5b
alvardsoler <aigam666@gmail.com>
2016-01-27 15:25:38 +0100

    Pasa nuestras pruebas pero falla en el caso 7 30 15 150 90 500 263 5, devolvemos 680 y deberiamos 685

--- tema5/ejer34/main.cpp
+++ tema5/ejer34/main.cpp
@@ -1,17 +1,17 @@
-/* 
- *
- * TAIS 16 Álvar Soler Rus y Samuel Lapuente Jiménez 
- * Ejercicio 34
- *
- * Explicación:
- * cortes(i,j) := coste mínimo de cortar un liston de tamaño j desde i
- * 
- * Caso base:
- * cortes(i, i+1) = esfuerzo(i,i+1)
- * Caso recursivo:
- * cortes(i, j) = min { cortes(i,j) , (i < k < j) (cortes(i,k) + cortes(k+1, j))}
- *
- */
+/*
+*
+* TAIS 16 Álvar Soler Rus y Samuel Lapuente Jiménez
+* Ejercicio 34
+*
+* Explicación: comer(i,j) cantidad máxima que come Listilla de los cubos desde el cubo i hasta el cubo j
+*
+* CB par -> comer(i,i+1) return max(cubos[i][i+1]
+* CB impar -> comer(i,i) return cubos[i]
+*
+* Recursión
+* comer(i, j) = max { cubos[i] + comer(i+1, j-1), cubos[j] + comer(i+1, j-1)}
+*
+*/
 
 #include <iostream>
 #include <cstdio>
@@ -22,28 +22,36 @@
 #include <algorithm>
 
 int comer(std::vector<int> const & cubos, int const n) {
-    Matriz<int> vacas(n, n, 0);
+    Matriz<int> vacas(n+1, n+1, 0);
 
     if (n == 1) return cubos[1];
     if (n == 2) return std::max(cubos[1], cubos[2]);
+    size_t f = 0;
+   
+    if (n % 2 == 0){ // pares
+        for (size_t i = 1; i < n; ++i)
+            vacas[i][i + 1] = std::max(cubos[i], cubos[i + 1]);
+        f = 3;
+    }
+    else{ //impares
+        for (size_t i = 1; i < n; ++i)
+            vacas[i][i] = cubos[i];
+        f = 2;       
+    }
 
-    for (size_t i = 1; i < n; ++i)
-	vacas[i][i + 1] = std::max(cubos[i], cubos[i + 1]);
+    for (size_t d = f; d < n; d += 2) {
+        for (size_t i = 1; i <= n - d; ++i) {
+            size_t j = i + d;
+            size_t comoi, comoj;
+            comoi = comoj = 0;
+            if (cubos[j] > cubos[i + 1]) comoi = vacas[i + 1][j - 1];
+            else comoi = vacas[i + 2][j];
 
+            if (cubos[i] > cubos[j - 1]) comoj = vacas[i + 1][j - 1];
+            else comoj = vacas[i][j - 2];
 
-    for (size_t d = 3; d < n; d += 2) {
-	for (size_t i = 1; i <= n - d; ++i) {
-	    size_t j = i + d;
-	    size_t comoi, comoj;
-	    comoi = comoj = 0;
-	    if (cubos[j] > cubos[i + 1]) comoi = vacas[i + 1][j - 1];
-	    else comoi = vacas[i + 2][j];
-
-	    if (cubos[i] > cubos[j - 1]) comoj = vacas[i + 1][j - 1];
-	    else comoj = vacas[i][j - 2];
-
-	    vacas[i][j] = std::max(cubos[i] + comoi, cubos[j] + comoj);
-	}
+            vacas[i][j] = std::max(cubos[i] + comoi, cubos[j] + comoj);
+        }
     }
 
     return vacas[1][n];
@@ -63,16 +71,16 @@ int main() {
     std::cin >> n;
 
     while (n != 0) {
-	std::vector<int> cubos(n + 1);
-	cubos[0] = 0;
-	for (int i = 1; i <= n; i++) {
-	    std::cin >> cubos[i];
-	}
+        std::vector<int> cubos(n + 1);
+        cubos[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            std::cin >> cubos[i];
+        }
 
 
-	std::cout << comer(cubos, n) << std::endl;
+        std::cout << comer(cubos, n) << std::endl;
 
-	std::cin >> n;
+        std::cin >> n;
     }
 
 #ifndef DOMJUDGE
@@ -86,4 +94,3 @@ int main() {
 
     return 0;
 }
-
