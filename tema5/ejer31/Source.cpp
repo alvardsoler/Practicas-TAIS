91c09a5837cb11291a0279f3680224b25be64a82
alvardsoler <aigam666@gmail.com>
2016-01-28 17:37:01 +0100

    Wrong answer

--- tema5/ejer31/Source.cpp
+++ tema5/ejer31/Source.cpp
@@ -1,7 +1,5 @@
 ﻿/*
- *TAIS16 Alvar David Soler Rus y Samuel Lapuente Jiménez ejercicio 31
- *
- * 
+ * TAIS16 Alvar David Soler Rus y Samuel Lapuente Jiménez ejercicio 31
  * 
  */
 
@@ -15,10 +13,12 @@
 
 /*
  * 
-monedas(i,j) = monedas(i-1,j) si vi > j
+ * monedas(i,j) = monedas(i-1,j) si vi > j
+ * 
+ * 
+ * 
+ * 
 monedas(i,j) = min(mon(i-1,j),mon(i-1,j - k*v[i])+k 1<=k<=cantida(i)
-
-
 si j<=tipo(i)
 monedas(i,j)=monedas(i-1,j);
 desde k=1 hasta cantidad(i){
@@ -26,16 +26,15 @@ desde k=1 hasta cantidad(i){
     if(temp < monedas(i,j)){
         monedas(i,j)=temp
 }
-
 }
 	
  */
 
 int monedas(std::vector<int> & monedas, std::vector<int> & cantidad, int const precio) {
-    size_t n = monedas.size();
+    size_t n = monedas.size() - 1;
     Matriz<int> matriz(n + 1, precio + 1, 1e9);
 
-    for (size_t i = 0; i <= n; ++i)
+    for (size_t i = 1; i <= n; ++i)
         matriz[i][0] = 0;
 
     for (size_t i = 1; i <= n; ++i) {
@@ -44,13 +43,15 @@ int monedas(std::vector<int> & monedas, std::vector<int> & cantidad, int const p
                 matriz[i][j] = matriz[i - 1][j];
             } else {
                 size_t temp = 0;
-                for (size_t k = 0; k <= cantidad[i]; ++k) {
-                    if (j - k * monedas[i] >= 0 && j - k * monedas[i] < precio + 1) {
-                        temp = matriz[i - 1][j - k * monedas[i]] + k;
-                        if (temp < matriz[i][j])
-                            matriz[i][j] = temp;
-                    }
+                int aux = (int) ((int) precio / (int) monedas[i]);
+                if (aux > cantidad[i]) aux = cantidad[i];
+
+                for (size_t k = 0; k <= aux; ++k) {
+                    temp = matriz[i - 1][j - k * monedas[i]] + k;
+                    if (temp < matriz[i][j])
+                        matriz[i][j] = temp;
                 }
+
             }
         }
     }
@@ -71,10 +72,14 @@ int main() {
     std::cin >> n;
     while (std::cin) {
         std::vector<int> m(n + 1), cantidades(n + 1);
-        for (size_t i = 0; i < n; ++i) std::cin >> m[i];
-        for (size_t i = 0; i < n; ++i) std::cin >> cantidades[i];
+        for (size_t i = 1; i <= n; ++i) std::cin >> m[i];
+        for (size_t i = 1; i <= n; ++i) std::cin >> cantidades[i];
         std::cin >> C;
-        std::cout << monedas(m, cantidades, C) << std::endl;
+        size_t r = monedas(m, cantidades, C);
+        if (r != 1e9)
+            std::cout << "SI " << r << std::endl;
+        else
+            std::cout << "NO" << std::endl;
         std::cin >> n;
     }
 
